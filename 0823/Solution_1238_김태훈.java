2c191b2026b064237a3150210d592d6edfbd4438
김태훈 <xogns5@naver.com>
2023-08-23 17:45:18 +0900

    Update 0823/Solution_1238_김태훈.java

--- 0823/Solution_1238_김태훈.java
+++ 0823/Solution_1238_김태훈.java
@@ -9,6 +9,19 @@ public class Solution_1238_김태훈 {
 	static boolean visit[];
 	static ArrayList<ArrayList<Integer>> graph;
 	
+	/*
+	 * T = 테스트 케이스, 10으로 고정
+	 * n = 최대가 100이고 연결 되는 경우도 있고 안되는 경우도 있고 해서 100으로 고정
+	 * edge = 입력 받은 간선
+	 * arr[] = 마지막에 넣은 값 다 저장, 저장하고 정렬해서 제일 큰 값 출력
+	 * 로직 => 입력 받은 선 graph 연결한다
+	 * 		bfs 똑같이 돌리면서 탐색하는데 같은 깊이에 있는 숫자들을 매번 저장해야됨
+	 * 		깊이를 알아야 하는데 while문 풀고 depht정보를 매개변수로 매번 돌리던지
+	 * 		같은 깊이에 있는 애들을 for문으로 돌리고 거기서 poll된 애들 저장하면 된다.
+	 * 		나는 for문으로 돌리고 배열에 저장. queue의 마지막이 아니라서 while문 다시돌면 저장 배열 초기화
+	 * 		다 끝나고 배열 정렬해서 제일 큰놈 출력
+	 */
+
 	static void init() throws IOException {
 		graph = new ArrayList<>();
 		st = new StringTokenizer(br.readLine());
