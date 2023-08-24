4a6cfaba594d2f80267c71beb7f4379ea9914ee5
김태훈 <xogns5@naver.com>
2023-08-24 22:37:06 +0900

    Upload New File

---
+++ 0824/Main_15961_김태훈.java
@@ -0,0 +1,12 @@
+package baekjoon;
+import java.util.*;
+import java.io.*;
+public class Main_15961_김태훈 {
+	static void init() throws IOException{
+		
+	}
+	public static void main(String[] args) throws IOException{
+		
+	}
+
+}
